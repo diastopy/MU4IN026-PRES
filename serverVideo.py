d8cba736a4beb8c2e1cd524f09bac60148739844
faotix <faotix@github.com>
2025-03-24 12:28:04 +0100

    AAAAAAAAAAAAH

--- serverVideo.py
+++ serverVideo.py
@@ -61,4 +61,4 @@ def serve_dash(filename):
     return send_from_directory('static/dash', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
